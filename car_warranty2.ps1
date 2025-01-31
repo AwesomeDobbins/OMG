4e87e553b0b57b4ce1736feaa4df8aaf0a78f56b
AwesomeDobbins <mike@netcommky.com>
2025-01-31 16:17:10 -0500

    Add files via upload

---
+++ car_warranty2.ps1
@@ -0,0 +1,14 @@
+# Add Speech Synthesis Assembly
+Add-Type -AssemblyName System.speech
+
+# Start a Background Job for Speech
+Start-Job -ScriptBlock {
+    Add-Type -AssemblyName System.speech
+    $speak = New-Object System.Speech.Synthesis.SpeechSynthesizer
+    $speak.Speak("We have been trying to reach you concerning your vehicle's extended warranty. 
+    You should have received a notice in the mail about your car's extended warranty eligibility. 
+    Since we have not gotten a response, we are giving you a final courtesy call before we close out your file. 
+    Press 2 to be removed and placed on our do-not-call list. 
+    To speak to someone about possibly extending or reinstating your vehicle's warranty, 
+    press 1 to speak with a warranty specialist.")
+} | Out-Null
